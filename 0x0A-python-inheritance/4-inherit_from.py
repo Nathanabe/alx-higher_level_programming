9fdaab9a6dbe2b1d7a7ebd14690a0e07a7a3c02c
Nathanabe <99434652+Nathanabe@users.noreply.github.com>
2022-07-05 00:50:45 +0100

    Update 4-inherit_from.py

--- 0x0A-python-inheritance/4-inherit_from.py
+++ 0x0A-python-inheritance/4-inherit_from.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 """
-Module 4-inherits_from.py
-Contains method inherits_from
-returns True if obj is instance of class that it inherits from or is subcls of
+module only sub class of
 """
 
 
 def inherits_from(obj, a_class):
+    """returns True if the object is an instance of a class
+       that inherited (directly or indirectly) from the specified class;
+       otherwise False.
     """
-    Notes:
-        use type() to get specific class
-        use isinstance() to get class and any parent classes too
-        use issubclass() to get what object is a subclass of
-    Return:
-        True if obj is instance of class that it inherits from or is subcls of
-    """
-    return (type(obj) is not a_class and issubclass(type(obj), a_class))
+    if type(obj) != a_class:
+        return issubclass(type(obj), a_class)
+    else:
+        return False
