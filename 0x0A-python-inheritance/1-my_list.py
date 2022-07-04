599df8ba3b1c4fb168e46d9110350a6a7beed92a
Nathanabe <99434652+Nathanabe@users.noreply.github.com>
2022-07-05 00:20:19 +0100

    my list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""class MyList that inherits from list
+"""
+Module 1-my_list
+Contains class MyList
+inherits from list; has public instance method to print sorted
 """
 
 
 class MyList(list):
-    """inherits from list"""
+    """inherits from list
+    methods:
+    print_sorted(self)
+    """
     def print_sorted(self):
-        """prints the list, but sorted
-        (ascending sort)
-        """
+        """prints list of ints all sorted in ascending order"""
         print(sorted(self))
