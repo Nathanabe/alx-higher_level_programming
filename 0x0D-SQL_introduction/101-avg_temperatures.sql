ea60a0aaff25889666725ed505ec1f6087e4d80f
nathanabe <nathanieladehor4@gmail.com>
2022-08-09 17:58:44 -0700

    Temperature #

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,6 @@
+-- Displaying the average tepertature (in fahrenheit)
+-- by city ordered by descending temperature.
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp` DESC;
