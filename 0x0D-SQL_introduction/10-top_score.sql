c17020f2f87638af00718b21dc65dfe2e7804be1
nathanabe <nathanieladehor4@gmail.com>
2022-08-09 17:01:04 -0700

    Top scores updated

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,5 @@
-- lists all records of second_table ordered by score
-SELECT score, name FROM second_table ORDER BY score DESC;
+-- Lists all records of all the table second_table.
+-- Records all ordered by descending score.
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
