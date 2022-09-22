f1d6c24e72d1dd0ac6bb384f56f002ba3da491ab
Nathanabe <nathanieladehor@gmail.com>
2022-09-22 23:51:53 +0100

    Updated

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,35 +1,16 @@
 #!/usr/bin/python3
+"""contains the function find_peak"""
 
 
 def find_peak(list_of_integers):
-    """Find a peak in a list of integers.
-    Example:
-        [1, 2, 4, 6, 3] -> 6
-        [4, 2, 1, 2, 3, 1] -> 3 # but 4 is also a peak
-    Returns: integer that is the peak.
-    """
-    l = len(list_of_integers)
-    n = l // 2
-    v = 0
-    while v < l:
-        v += 1
-        if n == 0:
-            gtl = True
-        else:
-            gtl = False
-        if n == l - 1:
-            gtr = True
-        else:
-            gtr = False
-        if n > 0:
-            if list_of_integers[n - 1] < list_of_integers[n]:
-                gtl = True
-        if n < l - 1:
-            if list_of_integers[n] > list_of_integers[n + 1]:
-                gtr = True
-        if gtl and gtr:
-            return list_of_integers[n]
-        if not gtr:
-            n += (l - n) // 2
-        elif not gtl:
-            n -= n // 2
+    """finds a peak in a list of unsorted integers"""
+    li = list_of_integers
+    l = len(li)
+    if l == 0:
+        return
+    m = l // 2
+    if (m == l - 1 or li[m] >= li[m + 1]) and (m == 0 or li[m] >= li[m - 1]):
+        return li[m]
+    if m != l - 1 and li[m + 1] > li[m]:
+        return find_peak(li[m + 1:])
+    return find_peak(li[:m])
