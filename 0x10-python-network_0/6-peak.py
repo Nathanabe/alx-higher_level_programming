f782b521ac01021bfcd3776a799c72769f6f0f1c
Nathanabe <nathanieladehor@gmail.com>
2022-09-23 00:04:36 +0100

    peak file

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -13,4 +13,4 @@ def find_peak(list_of_integers):
         return li[m]
     if m != l - 1 and li[m + 1] > li[m]:
         return find_peak(li[m + 1:])
-    return find_peak(li[:m])
+    return find_peak(li[:m]\n)
