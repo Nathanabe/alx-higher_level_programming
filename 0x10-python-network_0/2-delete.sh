8980b7dff7dbd679f4073f2d2c77bdaf28b8f11e
Nathanabe <nathanieladehor@gmail.com>
2022-09-23 00:29:20 +0100

    updated code

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send DELETE request to listed URL
-curl -sX DELETE "$1
+# sends a DELETE request to the URL passed as the first argument and displays the body of the response
+curl -sX DELETE "$1"
